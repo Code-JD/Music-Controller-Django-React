ff1e41715610cec488d80298831f6c77cd572653
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-11-24 21:28:00 -0800

    added create a room, play/pause, and votes

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { FormHelperText } from '@material-ui/core';
 
 export default class CreateRoomPage extends Component {
+	defaultVotes = 2;
+	
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return <p>This is the Create Room Page</p>
+		return (
+			<Grid container spacing={1}>
+				<Grid item xs={12} align="center">
+					<Typography component="h4" variant="h4">
+						Create a Room
+					</Typography>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<FormControl component="fieldset">
+						<FormHelperText>
+							<div align="center">Guest Control of Playback State</div>
+						</FormHelperText>
+						<RadioGroup row defaultValue="true">
+							<FormControlLabel
+								value='true'
+								control={<Radio color="primary" />}
+								label = "Play/Pause"
+								labelPlacement="bottom"
+							/>
+							<FormControlLabel
+								value='false'
+								control={<Radio color="secondary" />}
+								label = "No Control"
+								labelPlacement="bottom"
+							/>
+						</RadioGroup>
+					</FormControl>
+				</Grid>
+					<Grid item xs={12} align="center">
+						<TextField
+							required={true}
+							type="number"
+							defaultValue={this.defaultVotes}
+							inputProps={{
+								min: 1,
+							}}
+						/>
+						<FormHelperText>
+							<div align="center">Votes Required To Skip Song</div>
+						</FormHelperText>
+					</Grid>
+			</Grid>
+		);
 	}
 }
